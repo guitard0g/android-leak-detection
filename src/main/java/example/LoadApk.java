3d89d44bf39a4d454b753c8f7ba8ee9523a33aa1
jjrylearn <joshuaryanlearn@gmail.com>
2019-04-21 17:07:10 -0400

    finished translated LoadApk example

--- src/main/java/example/LoadApk.java
+++ src/main/java/example/LoadApk.java
@@ -1,14 +1,22 @@
 package example;
 
 import org.argus.amandroid.alir.componentSummary.ApkYard;
+import org.argus.amandroid.core.appInfo.ApkCertificate;
 import org.argus.amandroid.core.decompile.DecompileLayout;
 import org.argus.amandroid.core.decompile.DecompileStrategy;
 import org.argus.amandroid.core.decompile.DecompilerSettings;
+import org.argus.amandroid.core.parser.ComponentInfo;
+import org.argus.amandroid.core.parser.IntentFilterDataBase;
 import org.argus.amandroid.core.ApkGlobal;
 import org.argus.jawa.core.DefaultReporter;
+import org.argus.jawa.core.JawaType;
+import org.argus.jawa.core.Signature;
 import org.argus.amandroid.core.AndroidConstants;
 import org.argus.jawa.core.DefaultLibraryAPISummary;
 import org.argus.jawa.core.util.FileUtil;
+import scala.collection.immutable.Set;
+import scala.collection.immutable.Map;
+import scala.Tuple2;
 
 class LoadApk {
     public static void main(String[] args){
@@ -38,33 +46,12 @@ class LoadApk {
                                                              tempSettings.$lessinit$greater$default$7());
 
         ApkGlobal apk = yard.loadApk(fileUri, settings, false, false, yard.loadApk$default$5());
+
+        String appName = apk.model().getAppName();
+        Set<ApkCertificate> certificates = apk.model().getCertificates();
+        Set<String> uses_permissions = apk.model().getUsesPermissions();
+        Set<ComponentInfo> component_infos = apk.model().getComponentInfos();
+        IntentFilterDataBase intent_filter = apk.model().getIntentFilterDB();
+        Map<JawaType, Tuple2<Signature, String>> environment_map = apk.model().getEnvMap();
     }
 }
-/* TRANSLATING FROM:
-
-class LoadApkScala {
-  def main(args: Array[String]): Unit = {
-    if(args.length != 2) {
-      println("usage: apk_path output_path")
-      return
-    }
-    val fileUri = FileUtil.toUri(args(0))
-    val outputUri = FileUtil.toUri(args(1))
-    val reporter = new DefaultReporter
-    // Yard is the apks manager
-    val yard = new ApkYard(reporter)
-    val layout = DecompileLayout(outputUri)
-    val strategy = DecompileStrategy(layout)
-    val settings = DecompilerSettings(debugMode = false, forceDelete = true, strategy, reporter)
-    // apk is the apk meta data manager, class loader and class manager
-    val apk = yard.loadApk(fileUri, settings, collectInfo = false, resolveCallBack = false)
-
-    val appName = apk.model.getAppName
-    val certificate = apk.model.getCertificates
-    val uses_permissions = apk.model.getUsesPermissions
-    val component_infos = apk.model.getComponentInfos // ComponentInfo(compType: [class type], typ: [ACTIVITY, SERVICE, RECEIVER, PROVIDER], exported: Boolean, enabled: Boolean, permission: ISet[String])
-    val intent_filter = apk.model.getIntentFilterDB // IntentFilterDB contains intent filter information for each component.
-    val environment_map = apk.model.getEnvMap // environment method map
-  }
-}
- */
